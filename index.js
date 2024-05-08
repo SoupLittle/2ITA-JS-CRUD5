9eee832a4ac5b7fcf49ca297ba8dbc2df7e151ca
SoupLittle <marleneturlid@gmail.com>
2024-05-08 08:49:25 +0200

    *

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import { getDatabase, ref, set  } from "https://www.gstatic.com/firebasejs/10.11
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+var firebaseConfig = {
     apiKey: "AIzaSyCv9iZBmukWyh9V2hdKrVdG3aSIjZnvhYY",
     authDomain: "js-crud-5-b0981.firebaseapp.com",
     databaseURL: "https://js-crud-5-b0981-default-rtdb.firebaseio.com/",
@@ -20,41 +20,41 @@ const firebaseConfig = {
 
   
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const database = getDatabase(app); 
+var app = initializeApp(firebaseConfig);
+var analytics = getAnalytics(app);
+var database = getDatabase(app); 
 
 // Game logic
-const options = ["Rock", "Paper", "Scissors"];
+var options = ["Rock", "Paper", "Scissors"];
 
 document.querySelectorAll('.options button').forEach(button => {
   button.addEventListener('click', () => {
-    const playerChoice = button.id;
-    const computerChoice = options[Math.floor(Math.random() * options.length)];
-    const result = getResult(playerChoice, computerChoice);
+    var playerChoice = button.id;
+    var computerChoice = options[Math.floor(Math.random() * options.length)];
+    var result = getResult(playerChoice, computerChoice);
     document.getElementById('result').innerText = `Player: ${playerChoice} | Computer: ${computerChoice} | ${result}`;
  
     
     // Save result to Firebase
     
-    const resultsRef = ref(database, 'Results'); 
-    const newResultRef = ref(database, `Results/${generateNewResultKey()}`);
+      set(newResultRef, {"Player": playerChoice, "Computer": computerChoice, "Result": result})
+        .then(() => {
+          console.log("Data stored successfully");
+      })
 
-set(newResultRef, {"Player": playerChoice, "Computer": computerChoice, "Result": result})
-  .then(() => {
-    console.log("Data stored successfully");
-  })
-  .catch((error) => {
-    console.error("Error:", error);
-    alert("Unsuccessful, error" + error);
+      .catch((error) => {
+        console.error("Error:", error);
+        alert("Unsuccessful, error" + error);
+      });
+
+      var newResultRef = ref(database, `Results/${generateNewResultKey()}`);
+
+        function generateNewResultKey() {
+
+        return Date.now().toString();
+       } 
+    });
   });
-
-function generateNewResultKey() {
-
-  return Date.now().toString();
-    } 
-  });
-});
 
 
 //Result text
